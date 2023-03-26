c997af87871f87f837e35b2a1107869eb9c52935
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-03-26 19:08:34 +0200

    fixes

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 function toggleDropdown() {
-    const dropdown = document.getElementById("myDropdown");
+    const dropdown = document.getElementsByClassName("dropdown-content");
     dropdown.classList.toggle("show");
-}
-
-window.onclick = (event) => {
-    if (!event.target.matches('.dropbtn')) {
-        const dropdowns = document.getElementsByClassName("dropdown-content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+    window.onclick = (event) => {
+        if (!event.target.matches('.dropbtn')) {
+            const dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
             }
         }
     }
+}
+
+function toggle() {
+    const hamburger = document.querySelector('.hamburger');
+    const menuList = document.querySelector('.menu-list');
+
+    let menuOpen = false;
+
+    hamburger.addEventListener('click', function() {
+        if (!menuOpen) {
+            menuList.style.display = 'block';
+            menuOpen = true;
+        } else {
+            menuList.style.display = 'none';
+            menuOpen = false;
+        }
+    });
 }
